11bd3e9a6ffddb9be5e8b09878f269323e89762d
Lucas Kent <rubickent@gmail.com>
2024-04-27 17:06:42 +1000

    Remove unused "Inspector" button

--- executable-visualizer-lib/src/app.rs
+++ executable-visualizer-lib/src/app.rs
@@ -1,20 +1,11 @@
 use crate::file_loader::FileLoader;
 use crate::sections::ExecutableFile;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Default)]
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
-pub enum View {
-    #[default]
-    Inspector,
-}
-
 #[derive(Default)]
 pub struct ExampleApp {
     /// Options for configuring how the Inspector is displayed.
     pub inspector_options: crate::inspector::Options,
 
-    /// What view is active.
-    pub view: View,
     pub files: Vec<ExecutableFile>,
     file_loader: FileLoader,
 }
@@ -23,7 +14,6 @@ impl ExampleApp {
     pub fn new(files: Vec<ExecutableFile>) -> Self {
         ExampleApp {
             inspector_options: Default::default(),
-            view: Default::default(),
             files,
             file_loader: FileLoader::default(),
         }
@@ -47,17 +37,9 @@ impl eframe::App for ExampleApp {
                 self.files.push(file);
             }
 
-            ui.horizontal(|ui| {
-                ui.selectable_value(&mut self.view, View::Inspector, "Inspector");
-            });
-
             ui.separator();
 
-            match self.view {
-                View::Inspector => {
-                    crate::inspector::ui(ui, &mut self.inspector_options, &mut self.files)
-                }
-            }
+            crate::inspector::ui(ui, &mut self.inspector_options, &mut self.files)
         });
     }
 }
